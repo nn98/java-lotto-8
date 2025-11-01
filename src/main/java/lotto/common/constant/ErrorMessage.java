88cf9ff2d9c24bd63e17ed1aa75350778d08f1fe
Andrew Kim <38521355+nn98@users.noreply.github.com>
2025-11-02 01:09:23 +0900

    feat(errorMsg): 공통 양수 입력 예외 메시지 추가'

--- src/main/java/lotto/common/constant/ErrorMessage.java
+++ src/main/java/lotto/common/constant/ErrorMessage.java
@@ -3,6 +3,9 @@ package lotto.common.constant;
 public enum ErrorMessage {
 
     INVALID_INPUT("[ERROR] 입력값은 공백이 아니어야 합니다."),
+    NON_POSITIVE_INPUT("[ERROR] 입력값은 양의 정수여야 합니다."),
+    NON_NUMERIC_INPUT("[ERROR] 입력값은 숫자여야 합니다."),
+
     INVALID_AMOUNT("[ERROR] 구입금액은 양의 정수여야 합니다."),
     INDIVISIBLE_AMOUNT("[ERROR] 구입금액은 1000으로 나누어 떨어지는 값이어야 합니다."),
 
