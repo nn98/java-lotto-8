7c615019eb7f024f5fee096aa3e632b03b317c06
Andrew Kim <38521355+nn98@users.noreply.github.com>
2025-11-02 05:04:23 +0900

    feat(controller): 기본 생성자에서 의존성 주입

---
+++ src/main/java/lotto/presentation/controller/LottoController.java
@@ -0,0 +1,16 @@
+package lotto.presentation.controller;
+
+import lotto.presentation.view.InputView;
+import lotto.presentation.view.OutputView;
+
+public class LottoController {
+
+    private final InputView inputView;
+    private final OutputView outputView;
+
+    public LottoController(InputView inputView, OutputView outputView) {
+        this.inputView = inputView;
+        this.outputView = outputView;
+    }
+
+}
