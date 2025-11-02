d330717373c9e8e9c65ca6983e6c3bcdf3fc2504
Andrew Kim <38521355+nn98@users.noreply.github.com>
2025-11-03 01:07:37 +0900

    feat(service): 구입 금액으로 로또 발행 구현

---
+++ src/main/java/lotto/service/LottoService.java
@@ -0,0 +1,29 @@
+package lotto.service;
+
+import camp.nextstep.edu.missionutils.Randoms;
+import java.util.ArrayList;
+import java.util.List;
+import lotto.domain.Lotto;
+import lotto.domain.UserLotteries;
+
+public class LottoService {
+
+    private int amount;
+    private UserLotteries userLotteries;
+
+    public LottoService() {
+    }
+
+    public void issuanceLotteries(int amount) {
+        List<Lotto> lotteries = new ArrayList<>();
+        for (int i = amount / 1000 ; i > 0 ; i--) {
+            lotteries.add(issuanceLotto());
+        }
+        this.userLotteries = new UserLotteries(lotteries);
+    }
+
+    private Lotto issuanceLotto() {
+        return new Lotto(Randoms.pickUniqueNumbersInRange(1, 45, 6));
+    }
+
+}
