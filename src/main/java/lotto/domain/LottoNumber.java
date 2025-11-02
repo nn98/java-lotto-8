90bdf664a8beb8e63869f3c178d6c11ba464d30e
Andrew Kim <38521355+nn98@users.noreply.github.com>
2025-11-02 23:48:03 +0900

    feat(lottoNumber): 숫자를 기반으로 equals 비교 구현

--- src/main/java/lotto/domain/LottoNumber.java
+++ src/main/java/lotto/domain/LottoNumber.java
@@ -1,10 +1,42 @@
 package lotto.domain;
 
-public class LottoNumber {
+import java.util.Objects;
+
+public class LottoNumber implements Comparable<LottoNumber> {
+
+    private final int number;
+
     public LottoNumber(int number) {
+        this.number = number;
     }
 
     public int getNumber() {
         return 0;
     }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(number);
+    }
+
+    @Override
+    public boolean equals(Object obj) {
+        if (this == obj)
+            return true;
+        if (obj == null || getClass() != obj.getClass())
+            return false;
+        LottoNumber that = (LottoNumber)obj;
+        return number == that.number;
+    }
+
+    @Override
+    public String toString() {
+        return String.valueOf(number);
+    }
+
+    @Override
+    public int compareTo(LottoNumber lottoNumber) {
+        return Integer.compare(this.number, lottoNumber.number);
+    }
+
 }
