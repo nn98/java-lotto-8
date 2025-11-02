30ce708e8a04c148b812fbfee174a74fed06c5a0
Andrew Kim <38521355+nn98@users.noreply.github.com>
2025-11-02 23:55:22 +0900

    feat(lottoNumber): validate 구현, 기본 기능 완료

--- src/main/java/lotto/domain/LottoNumber.java
+++ src/main/java/lotto/domain/LottoNumber.java
@@ -1,13 +1,24 @@
 package lotto.domain;
 
 import java.util.Objects;
+import lotto.common.constant.ErrorMessage;
 
 public class LottoNumber implements Comparable<LottoNumber> {
+
+    private static final int MIN_BOUND = 1;
+    private static final int MAX_BOUND = 45;
 
     private final int number;
 
     public LottoNumber(int number) {
+        validateNumber(number);
         this.number = number;
+    }
+
+    private void validateNumber(int number) {
+        if (number < MIN_BOUND || number > MAX_BOUND) {
+            throw new IllegalArgumentException(ErrorMessage.INVALID_NUMBER.getMessage());
+        }
     }
 
     public int getNumber() {
