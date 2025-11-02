58a546637b7e592d19b9126699bce42c5b66c989
Andrew Kim <38521355+nn98@users.noreply.github.com>
2025-11-03 00:05:14 +0900

    feat(errorMsg): 로또 번호 6개 미준수 메시지 추가(기본 코드에서 이관)

--- src/main/java/lotto/common/constant/ErrorMessage.java
+++ src/main/java/lotto/common/constant/ErrorMessage.java
@@ -10,6 +10,7 @@ public enum ErrorMessage {
     INDIVISIBLE_AMOUNT("[ERROR] 구입금액은 1000으로 나누어 떨어지는 값이어야 합니다."),
 
     INVALID_NUMBER("[ERROR] 로또 번호는 1부터 45 사이의 양의 정수여야 합니다."),
+    INVALID_NUMBER_COUNT("[ERROR] 로또 번호는 6개여야 합니다."),
     DUPLICATED_NUMBER("[ERROR] 로또 번호는 중복되지 않는 값이어야 합니다.");
 
     private final String message;
