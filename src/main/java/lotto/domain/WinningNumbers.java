f176454060f70134bcd01afb6148fcf0d1d8614d
Andrew Kim <38521355+nn98@users.noreply.github.com>
2025-11-03 00:10:20 +0900

    feat(winningNumbers): 기본 생성자 로직 구현

--- src/main/java/lotto/domain/WinningNumbers.java
+++ src/main/java/lotto/domain/WinningNumbers.java
@@ -4,7 +4,12 @@ import java.util.List;
 
 public class WinningNumbers {
 
+    private final Lotto lotto;
+    private final int bonusNumber;
+
     public WinningNumbers(List<Integer> numbers, int bonusNumber) {
+        this.lotto = new Lotto(numbers);
+        this.bonusNumber = bonusNumber;
     }
 
     public Winning determineWinning(Lotto userLotto) {
