b96524abc609f07f7a9cf21bfff9a5c5f3d07648
Andrew Kim <38521355+nn98@users.noreply.github.com>
2025-11-03 19:33:13 +0900

    feat(winning): 당첨 통계 출력을 위한 설명 필드 추가, 통계 포맷 반환 메서드 추가

--- src/main/java/lotto/domain/Winning.java
+++ src/main/java/lotto/domain/Winning.java
@@ -4,21 +4,23 @@ import java.util.Arrays;
 
 public enum Winning {
 
-    LOSING(0,false, 0),
-    FIFTH(3,false, 5000),
-    FOURTH(4,false, 50000),
-    THIRD(5,false, 1500000),
-    SECOND(5,true, 30000000),
-    FIRST(6,false, 2000000000);
+    LOSING(0,false, 0, "꽝"),
+    FIFTH(3,false, 5000, "3개 일치"),
+    FOURTH(4,false, 50000, "4개 일치"),
+    THIRD(5,false, 1500000, "5개 일치"),
+    SECOND(5,true, 30000000, "5개 일치, 보너스 볼 일치"),
+    FIRST(6,false, 2000000000, "6개 일치");
 
     private final int matchCount;
     private final boolean isBonusNumberMatched;
     private final int prize;
+    private final String description;
 
-    Winning(int matchCount, boolean isBonusNumberMatched, int prize) {
+    Winning(int matchCount, boolean isBonusNumberMatched, int prize, String description) {
         this.matchCount = matchCount;
         this.isBonusNumberMatched = isBonusNumberMatched;
         this.prize = prize;
+        this.description = description;
     }
 
     public static Winning valueOf(int matchCount, boolean isBonusNumberMatched) {
@@ -31,6 +33,10 @@ public enum Winning {
 
     public int calcPrize(int matchCount) {
         return this.prize * matchCount;
+    }
+
+    public String getStatisticsFormat(int count) {
+        return String.format("%s (%,d원) - %d개", description, prize, count);
     }
 
 }
